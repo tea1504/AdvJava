4b2db5a7da7b1ff151987697d89f9bd653ff4393
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-29 09:14:57 +0700

    Thêm nữa

--- src/main/java/advjava/bean/Lop.java
+++ src/main/java/advjava/bean/Lop.java
@@ -3,4 +3,16 @@ package advjava.bean;
 public class Lop {
 	private String MaLop;
 	private String TenLop;
+	public String getMaLop() {
+		return MaLop;
+	}
+	public void setMaLop(String maLop) {
+		MaLop = maLop;
+	}
+	public String getTenLop() {
+		return TenLop;
+	}
+	public void setTenLop(String tenLop) {
+		TenLop = tenLop;
+	}
 }
