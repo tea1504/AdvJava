80ef49d346ceac51da016180a260eb9d1c1c8c93
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-29 10:06:14 +0700

    Khởi tạo chức năng quản lý phòng máy

---
+++ src/main/java/advjava/DAO/PhongDAO.java
@@ -0,0 +1,5 @@
+package advjava.DAO;
+
+public class PhongDAO {
+
+}
--- src/main/java/advjava/bean/Phong.java
+++ src/main/java/advjava/bean/Phong.java
@@ -3,4 +3,5 @@ package advjava.bean;
 public class Phong {
 	private String MaPhong;
 	private String TenPhong;
+	private int SoMay;
 }
