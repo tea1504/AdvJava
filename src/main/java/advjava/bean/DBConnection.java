68c29591ed4b58f4101f6627e53d07c5dedb0004
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-29 10:05:52 +0700

    Thêm file kết nối CSDL

---
+++ src/main/java/advjava/bean/DBConnection.java
@@ -0,0 +1,76 @@
+package advjava.bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnection {
+	private Connection conn;
+	/**
+	 * Lưu mật khẩu kết nối đến CSLDL MySQL
+	 */
+	static final String PASSWORD = "tn208";
+	static final String USERNAME = "javanc";
+	static final String URL = "jdbc:mysql://localhost:3308/ql_phongmay";
+
+	/**
+	 * Ham khoi tao
+	 */
+	public DBConnection() {
+		try {
+			Class.forName("jdbc.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Hàm dùng để lấy kết nối để kết nối đến MySQL
+	 * 
+	 * @return Một kết nối
+	 */
+	public Connection getConnection() {
+		try {
+			conn = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	/**
+	 * Đóng một kết nối
+	 */
+	public void closeConnection() {
+		if (conn == null) {
+			return;
+		} else {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param query
+	 * @return
+	 * 	null trong trường hợp lỗi<br>
+	 * 	tập kết quả
+	 */
+	public ResultSet excuted(String query) {
+		PreparedStatement ps;
+		ResultSet rs;
+		try {
+			ps = conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			return rs;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
